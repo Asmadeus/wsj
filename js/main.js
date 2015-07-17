9392fa7dd195c162f5b8c51eb51d136118643944
Kirill Grevtsov <kgrevtsov@mail.ru>
2015-07-17 13:29:48 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready( function() {
+  $('header').addClass('search-closed');
+
+  $('.search-icon').click( function() {
+    if ($('header').hasClass('search-closed')) {
+      $('header').removeClass('search-closed').addClass('search-open');
+      $('body').addClass('no-scroll');
+      if ($( document ).width() < 1000) {
+        var mask = document.createElement('div');
+        $(mask).css ({
+          'position' : 'absolute',
+          'left' : '0',
+          'width' : '100%',
+          'height' : $( document ).height()-77,
+          'background-color' : 'black',
+          'opacity' : '0.75',
+          'top' : '78px'
+        });
+        $(mask).addClass('mask');
+        $('header').append(mask);
+      }
+    }
+    else {
+      $('header').removeClass('search-open').addClass('search-closed');
+      $('.mask').remove();
+      $('body').removeClass('no-scroll');
+    }
+  });
+
+  $('ul.section-list').addClass('cursor-out');
+  $('ul.section-list li').hover(
+    function() {
+      $(this).parent().removeClass('cursor-out').addClass('cursor-in');
+    },
+    function() {
+      $(this).parent().removeClass('cursor-in').addClass('cursor-out');
+    }
+  );
+});
